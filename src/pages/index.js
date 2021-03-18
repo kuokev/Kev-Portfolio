e4119a6683aea837edc282055e74175d6e4c1b23
Kevin Kuo <kuokev@uw.edu>
2021-03-18 00:11:14 -0700

    Update homepage image.

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Helmet from "react-helmet";
 import Layout from "components/Layout";
 import Container from "components/Container";
 
-import img_gatsby from "assets/images/cover.png";
+import img_gatsby from "assets/images/kevin_bird.png";
 
 const IndexPage = () => {
   return (
@@ -13,11 +13,11 @@ const IndexPage = () => {
         <title>Kevin | HCDE @ UW</title>
       </Helmet>
       <Container>
-        {/* <p className="cover">
-          <img src={img_gatsby} alt="Profile Picture would be here" />
-        </p> */}
+        <p className="cover">
+          <img src={img_gatsby} alt="Image of a bird that shares the name of Kevin" />
+        </p>
         <h1 className="heading">Hello! I'm Kevin</h1>
-        <p>Welcome to your new site.</p>
+        <p>[Add one of those cool fill-in-the-blank transition texts]</p>
       </Container>
     </Layout>
   );
